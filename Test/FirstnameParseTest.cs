038505269650c2695bbae931cf8258bc79ed6fc3
Benjamin Jensen <bj@autoit.dk>
2020-01-27 11:55:35 +0100

    Commit of existing project "Test"

---
+++ Test/FirstnameParseTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class FirstnameParseTest
+    {
+        private FirstnameParser _firstNameParser;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _firstNameParser = new FirstnameParser();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _firstNameParser.Parse("Du har modtaget et nyt lead. Når du kontakter leaded er der i nedenstående angivet en PIN-kode," +
+                                   " som skal benyttes når du ringer til Freespee på tlf. 70303322\r\n\r\nNavn: sarah\r\n");
+        }
+    }
+}
